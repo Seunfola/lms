2843d014e03ea29cff4ec6c02903e3a734863884
Folahan <seunfola1@gmail.com>
2024-06-09 12:13:24 +0100

    fixing production bug

--- app/api/ai/route.ts
+++ app/api/ai/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import OpenAI from "openai";
 
 const openai = new OpenAI({
-  apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 export async function POST(req: Request) {
