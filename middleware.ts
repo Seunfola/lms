5be87be160c960293bd438bf161ac3928de99406
Folahan O Mosunmola <seunfola1@gmail.com>
2024-12-16 13:54:17 +0100

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,6 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware({
-  publicRoutes: ["/"],
-  afterAuth: (auth, req, evt) => {
-    if (!auth.isPublicRoute && !auth.userId) {
-      return new Response("Unauthorized", { status: 401 });
-    }
-  },
-});
+export default authMiddleware({});
 
 export const config = {
   matcher: ["/((?!_next|.*\\..*).*)", "/", "/(api|trpc)(.*)"],
