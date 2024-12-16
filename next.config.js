53f58377b7f6b4c711c9fd87f5b878da6c7fc941
Seunfola <seunfola1@gmail.com>
2024-12-16 08:31:46 +0100

    updated

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        NEXT_PUBLIC_CLERK_FRONTEND_API: process.env.NEXT_PUBLIC_CLERK_FRONTEND_API,
-        CLERK_API_KEY: process.env.CLERK_API_KEY,
-        NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
-        PAYSTACK_SECRET_KEY: process.env.PAYSTACK_SECRET_KEY,
-        DATABASE_URL: process.env.DATABASE_URL,
-    },
+
     images: {
         remotePatterns: [
             {
