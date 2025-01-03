68387733e830740b4dcc9d06213e30585942203b
Seunfola <seunfola1@gmail.com>
2025-01-03 13:49:48 +0100

    working on the route swtiching for user and teachers

--- app/(dashboard)/(routes)/teacher/layout.tsx
+++ app/(dashboard)/(routes)/teacher/layout.tsx
@@ -1,17 +1,18 @@
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
-const TeacherLayout = ({ children }: { children: React.ReactNode }) => {
+const TeacherLayout = async ({ children }: { children: React.ReactNode }) => {
   const { userId: currentUserId } = auth();
 
   if (!currentUserId) {
-    return redirect("/");
+    redirect("/"); 
   }
 
-  return <div>
-            {children}
-          </div>;
+  return (
+    <div>
+      {children}
+    </div>
+  );
 };
 
 export default TeacherLayout;
-
